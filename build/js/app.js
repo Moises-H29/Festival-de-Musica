f94a32b1054bf417380b131c07db54bfa5c4048f
Moises Hernandez Pacheco <ironmoyher@outlook.com>
2025-01-13 00:41:04 -0600

    Resaltando la seccion actual al dar scroll

--- build/js/app.js
+++ build/js/app.js
@@ -1,6 +1,7 @@
 addEventListener('DOMContentLoaded', function() {
     navegacionFija();
     crearGaleria();
+    resaltarEnlace();
 })
 
 function navegacionFija() {
@@ -73,4 +74,27 @@ function cerrarModal() {
     setTimeout(() => {
         modal?.remove();
     }, 500);
+}
+
+function resaltarEnlace() {
+    document.addEventListener('scroll', function() {
+        const sections = document.querySelectorAll('section');
+        const navLinks = document.querySelectorAll('.navegacion-principal a');
+
+        let actual = '';
+        sections.forEach( section => {
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.clientHeight;
+            if(window.scrollY >= (sectionTop - sectionHeight / 3 ) ){
+                actual = section.id;
+            }
+        })
+
+        navLinks.forEach( link => {
+            link.classList.remove('active');
+            if(link.getAttribute('href') === '#' + actual){
+                link.classList.add('active');
+            }
+        })
+    })
 }
