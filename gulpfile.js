8e24c680ab0212af3547d890afb9c0bb67940a49
Moises Hernandez Pacheco <ironmoyher@outlook.com>
2025-01-17 23:57:02 -0600

    Uso de Sharp para imagenes (o algo asi)

--- gulpfile.js
+++ gulpfile.js
@@ -23,6 +23,34 @@ export function css( done ) {
     done()
 }
 
+export async function crop(done) {
+    const inputFolder = 'src/img/gallery/full'
+    const outputFolder = 'src/img/gallery/thumb';
+    const width = 250;
+    const height = 180;
+    if (!fs.existsSync(outputFolder)) {
+        fs.mkdirSync(outputFolder, { recursive: true })
+    }
+    const images = fs.readdirSync(inputFolder).filter(file => {
+        return /\.(jpg)$/i.test(path.extname(file));
+    });
+    try {
+        images.forEach(file => {
+            const inputFile = path.join(inputFolder, file)
+            const outputFile = path.join(outputFolder, file)
+            sharp(inputFile) 
+                .resize(width, height, {
+                    position: 'centre'
+                })
+                .toFile(outputFile)
+        });
+
+        done()
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 export function dev() {
     watch('src/scss/**/*.scss', css)  // Observa si hay cambios en el archivo y ejecuta la funcion css()
     watch('src/js/**/*.js', js)  
